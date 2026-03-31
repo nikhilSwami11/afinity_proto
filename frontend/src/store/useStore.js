b5832c596d04b2509be6eee8db77ce11591cc5ca
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-03-31 01:44:34 -0700

    deleting frontend documents from dev branch

--- frontend/src/store/useStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-  selectedUser: null,
-  setSelectedUser: (user) => set({ selectedUser: user }),
-  clearSelectedUser: () => set({ selectedUser: null }),
-}));
-
-export default useStore;
